09f9e6afbfb53bac46bfa5d5a1a705a95b2a450f
pGrimaud <pascalgrimaud@gmail.com>
2014-11-22 12:11:27 +0100

    remplacement de la table personne par la table exemple

--- bubux/bubux-archi/sql/mysql/createTable.sql
+++ bubux/bubux-archi/sql/mysql/createTable.sql
@@ -1,8 +1,10 @@
 use bubuxdb;
 
-create table personne(
-	id int not null auto_increment primary key,
-	nom varchar(50),
-	prenom varchar(50),
-	dateNaiss date
+create table EXEMPLE(
+	ID int not null unique auto_increment primary key,
+	CODE varchar(10) not null unique,
+	DESCRIPTION varchar(50),
+	TSTAMP timestamp not null
 );
+
+insert into EXEMPLE(CODE,DESCRIPTION) values('EX01','Description Exemple 01');
